6b5da235562d5ee348a1c69b73fd2db6d68036c6
Harrison <harrison@harrisons-air-3.devices.brown.edu>
2024-02-14 00:52:50 -0500

    feat - wrote a rough version of CSVHandler and Server that works with a few basic commands

---
+++ src/main/java/edu/brown/cs/student/main/Server/CSVHandler.java
@@ -0,0 +1,52 @@
+package edu.brown.cs.student.main.Server;
+
+import edu.brown.cs.student.main.CSVParser.CSVParser;
+import edu.brown.cs.student.main.CSVParser.Creators.CreateListFromRow;
+import edu.brown.cs.student.main.CSVParser.Search;
+import java.io.FileReader;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class CSVHandler implements Route {
+
+  private CSVParser parser;
+  private Search search;
+
+  public CSVHandler() {}
+
+  public Object handle(Request request, Response response) throws Exception {
+    String action = request.queryParams("action");
+    switch(action) {
+      case "loadcsv":
+        return this.loadcsv(request, response);
+      case "viewcsv":
+        return this.parser.getParsed();
+      case "searchcsv":
+        return this.searchcsv(request, response);
+      default:
+        return "Invalid action";
+    }
+  }
+
+  public Object loadcsv(Request request, Response response) throws Exception {
+    String path = request.queryParams("path");
+    try {
+      this.parser =
+          new CSVParser<>(
+          new FileReader(path),
+          new CreateListFromRow(),
+          true);
+      this.search = new Search(this.parser);
+      return "CSV loaded successfully";
+    } catch (Exception e) {
+      return "Failed to load CSV";
+    }
+  }
+
+  public Object searchcsv(Request request, Response response) throws Exception {
+    String query = request.queryParams("query");
+    return this.search.searchEveryColumn(query);
+  }
+
+}
---
+++ src/main/java/edu/brown/cs/student/main/Server/Server.java
@@ -0,0 +1,29 @@
+package edu.brown.cs.student.main.Server;
+
+import static spark.Spark.after;
+
+import spark.Spark;
+
+public class Server {
+  public static void main(String[] args) {
+    int port = 6969;
+    Spark.port(port);
+    after(
+        (request, response) -> {
+          response.header("Access-Control-Allow-Origin", "*");
+          response.header("Access-Control-Allow-Methods", "*");
+        });
+
+    /* example of commands i tested that worked:
+     * http://localhost:6969/csv?action=loadcsv&path=data/stars/stardata.csv
+     * http://localhost:6969/csv?action=viewcsv
+     * http://localhost:6969/csv?action=searchcsv&query=Bena
+     * http://localhost:6969/csv?action=searchcsv&query=1604
+     */
+    Spark.get("/csv", new CSVHandler());
+    Spark.init();
+    Spark.awaitInitialization();
+
+    System.out.println("Server started at http://localhost:" + port);
+  }
+}
